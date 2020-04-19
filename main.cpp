ffbf3faa38851d4e412f03025818e7a0a2366cd4
BrianDahi <brian_dahi@yahoo.com>
2020-04-19 12:05:14 -0500

    I created a multimap now trying to figure out how to reverse the order

--- main.cpp
+++ main.cpp
@@ -12,15 +12,14 @@ int main() {
     
   
     map<string, int> m;
+    map<string,int>::iterator it;
+  // map<int, string>::iterator it2;
     fstream myInputFile ;
    // myInputFile.open("input1.txt"); This is what gradescope requires
     myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1.txt");
     string token;
     char aChar;
-    char* arrayChar;
-    int i,j=0;
-
-    
+   
     while (!myInputFile.eof()){//Why the file is not end of file
         do{
             myInputFile.get(aChar);//get a char
@@ -30,23 +29,38 @@ int main() {
         
         if (!myInputFile.eof()){
             //This will clear the token of any existing declartion
-            
             token.clear();
             
             while ((!myInputFile.eof()) && ((aChar != ' ') && (aChar != '\n'))){
                 //This loop is the same as the last
                 
                 token += aChar;//Here we are puting aChar into a string
+                
                 myInputFile.get(aChar);// we get the next char
                 
             }
-            //Now I need to store into map
-            //Thinking of ways to store and increment
-            m[token] = 1;
-            cout<<"this is a token: "<<token<<endl;
+          m[token]++;
         }
     }
+    
+   // for (it =  m.begin(); it !=  m.end(); ++it) {//the m.begin() will start at the front of the map
+        // and the exit case will be m.end()
+     //  cout  <<"Key: " <<it->first<< " Value: " << it->second <<endl;
+    //}
     myInputFile.close();
+    
+    multimap<int,string> sortMap;
+    multimap<int, string> :: iterator it2;
+    for(it = m.begin(); it != m.end();++it){
+        sortMap.insert(make_pair(it->second, it->first));
+      
+    }
+  
+    for (it2 =  sortMap.begin(); it2 !=  sortMap.end(); ++it2) {//the m.begin() will start at the front of the map
+           // and the exit case will be m.end()
+          cout  <<"Key: " <<it2->second<< " Value: " << it2->first <<endl;
+       }
+   
     return 0;
 }
 
