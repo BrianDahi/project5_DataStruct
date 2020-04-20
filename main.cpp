b7709c5874522983b8ea7ff9f41c520c45ac2dfd
BrianDahi <brian_dahi@yahoo.com>
2020-04-19 23:21:43 -0500

    updated

--- main.cpp
+++ main.cpp
@@ -10,25 +10,26 @@ using namespace std;
 
 int main() {
     
-  
     map<string, int> m;
     map<string,int>::iterator it;
- 
+    char fileName[50];
+  //  cin.getline(fileName, 50);
     fstream myInputFile ;
-   // myInputFile.open("input1.txt"); This is what gradescope requires
+   // myInputFile.open(fileName);
+    
+   // myInputFile.open("input1.txt"); //This is what gradescope requires
    // myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1.txt");
     
-        myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1Real.txt");
+     //  myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1Real.txt");
     
-   //    myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input2.txt");
+      // myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input2.txt");
     
-   //     myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input3.txt");
+        myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input3.txt");
     
     string token;
     char aChar;
     
-
-    while(!myInputFile.eof()){
+  /*  while(!myInputFile.eof()){
         myInputFile.get(aChar);
          if (!myInputFile.eof()){
         token.clear();
@@ -38,11 +39,12 @@ int main() {
         }
          m[token]++;
         }
-    }
+    }*/
     
     
-   
-  /*  while (!myInputFile.eof()){//Why the file is not end of file
+    
+    
+    while (!myInputFile.eof()){//Why the file is not end of file
         do{
             myInputFile.get(aChar);//get a char
         }//end do
@@ -63,11 +65,13 @@ int main() {
             }//ends third while loop
           m[token]++;
         }//ends if statment
-    }//ends first loop*/
+    }//ends first loop
+    
+ 
     
     myInputFile.close();
     
-    multimap<int , string , greater<int>> sortMap;
+    multimap<int , string , greater<int> > sortMap;
     multimap<int, string> :: iterator it2;
     for(it = m.begin(); it != m.end();++it){
         
@@ -91,18 +95,18 @@ int main() {
     }
   
     /* Now we need to open the file again read and search and then print the position it is in*/
-    // myInputFile.open("input1.txt"); This is what gradescope requires
-    
+    // myInputFile.open("input1.txt"); //This is what gradescope requires
+   // myInputFile.open(fileName);
       // myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1.txt");
     
-           myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1Real.txt");
+         //  myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input1Real.txt");
     
-         //  myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input2.txt");
+         // myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input2.txt");
     
-         //  myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input3.txt");
+         myInputFile.open("/Users/briandahi/Documents/ou/Data Structures/Project5/Project5/input3.txt");
     i = 0;
     
-    while(!myInputFile.eof()){
+   /* while(!myInputFile.eof()){
         myInputFile.get(aChar);
          if (!myInputFile.eof()){
         token.clear();
@@ -118,36 +122,39 @@ int main() {
                           }
                }
          }
-    }
+    }*/
     
- /*   while (!myInputFile.eof()){//Why the file is not end of file
-           do{
-               myInputFile.get(aChar);//get a char
+    
+    
+    
+    while (!myInputFile.eof()){//Why the file is not end of file
+              do{
+                  myInputFile.get(aChar);//get a char
+              }
+              while((!myInputFile.eof()) && ((aChar == ' ') || (aChar == '\n')));
+              //while file is still not end And aChar does not ==  blank space or endline loop through.
+              
+              if (!myInputFile.eof()){
+                  //This will clear the token of any existing declartion
+                  token.clear();
+                  
+                  while ((!myInputFile.eof()) && ((aChar != ' ') && (aChar != '\n'))){
+                      //This loop is the same as the last
+                      
+                      token += aChar;//Here we are puting aChar into a string
+                      
+                      myInputFile.get(aChar);// we get the next char
+                      
+                  }
+              for(int i = 0; i < sortMap.size();++i){
+                         if(token == strArray[i]){
+                             cout<<i+1<<" ";
+                         }
+              }
+          
            }
-           while((!myInputFile.eof()) && ((aChar == ' ') || (aChar == '\n')));
-           //while file is still not end And aChar does not ==  blank space or endline loop through.
-           
-           if (!myInputFile.eof()){
-               //This will clear the token of any existing declartion
-               token.clear();
-               
-               while ((!myInputFile.eof()) && ((aChar != ' ') && (aChar != '\n'))){
-                   //This loop is the same as the last
-                   
-                   token += aChar;//Here we are puting aChar into a string
-                   
-                   myInputFile.get(aChar);// we get the next char
-                   
-               }
-           for(int i = 0; i < sortMap.size();++i){
-                      if(token == strArray[i]){
-                          cout<<i+1<<" ";
-                      }
-           }
-       
-        }
-       }*/
-  
+          }
+    
     myInputFile.close();
     return 0;
 }
